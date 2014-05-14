dd048489e19beb7c2993a8f0bdba14a008904d79
maloneyl <m@thinkmaloney.com>
2014-05-14 22:52:58 +0100

    game 1 done; still need to work on starting another game

--- script.js
+++ script.js
@@ -1,23 +1,88 @@
 /** Hangman **/
 
+function createNewGame() {
+  $.ajax({
+    type: 'POST',
+    url: 'http://hangman-api.herokuapp.com/hangman'
+  }).done(function(response) {
+    console.log(response); // e.g. Object {hangman: _____, token: "eySjf239gjs"}
+    initialDisplay(response);
+  }).fail(function(response) {
+    console.log(response);
+  });
+}
+
+function initialDisplay(response) {
+  updateHangmanWord(response);
+  updateGameToken(response);
+  $('#new-game').hide(); // hide 'Start a new game' button
+  $('#guess').show(); // show 'Guess' button
+  $('.letter').show(); // show guess input box
+  $('.remaining-guesses').show(); // show 'X guesses left'
+  // set X to 7
+  // clean up wrong guesses
+
+
+}
+
+function updateHangmanWord(response) {
+  $('.hangman-word').text(response.hangman);
+}
+
+function updateGameToken(response) {
+  $('.token').text(response.token);
+}
+
+function submitGuess() {
+  var game_token = $('.token').text(); // .val() doesn't work here
+  var guess = $('.letter').val();
+  $.ajax({
+    type: 'PUT',
+    url: 'http://hangman-api.herokuapp.com/hangman',
+    data: {
+      token: game_token,
+      letter: guess
+    }
+  }).done(function(response) {
+    if (response.correct) { // response.correct will be true or false
+      console.log("Yay!");
+      updateHangmanWord(response);
+      updateGameToken(response);
+    } else {
+      console.log("Boooo");
+      var wrong_attempts = $('.wrong').length + 1;
+      var wrong_html = '<span class="wrong">' + wrong_attempts + '</span>';
+      $('.attempts').append(wrong_html);
+      if (wrong_attempts == 7) {
+        retrieveSolution(game_token);
+      } else {
+        console.log("triggered");
+        $('.remaining').text(7 - wrong_attempts);
+      }
+    }
+  }).fail(function(response) {
+    console.log('AJAX ERROR', response);
+  })
+}
+
+function retrieveSolution(game_token) {
+  $.ajax({
+    type: 'GET',
+    url: 'http://hangman-api.herokuapp.com/hangman',
+    data: { token: game_token }
+  }).done(function(response) {
+    $('.hangman-word').text(response.solution);
+    $('.letter').hide();
+    $('#guess').hide();
+    $('.remaining-guesses').hide();
+    $('#new-game').show();
+    updateGameToken(response);
+  }).fail(function(response) {
+    console.log('AJAX ERROR', response);
+  })
+}
+
 $(document).ready(function() {
-
-  var resource_url = 'http://hangman-api.herokuapp.com/hangman';
-
   $('#new-game').on('click', createNewGame);
-
-  function createNewGame() {
-    $.ajax({
-      type: 'POST',
-      url: resource_url
-    }).done(function(data) {
-      console.log(data); // e.g. Object {hangman: _____, token: "eySjf239gjs"}
-      $('.hangman-word').text(data.hangman);
-      $('.token').text(data.token);
-      $('#new-game').hide();
-    }).fail(function(data) {
-      console.log(data);
-    });
-  }
-
+  $('#guess').on('click', submitGuess);
 });
